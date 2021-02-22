c21c2fcc62415ad734803fc9dda80d933df05a61
CAtSG <cli@studygroup.com>
2021-02-22 15:09:20 -0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,19 @@ import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
   return (
-      <>
-    <Hero>
-      <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
-        <Link to='/rooms' className='btn-primary'>
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
-    <Services/>
-    <FeaturedRooms/>
+    <>
+      <Hero>
+        <Banner
+          title="CLi's luxurious rooms"
+          subtitle='deluxe rooms starting at $299'
+        >
+          <Link to='/rooms' className='btn-primary'>
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
     </>
   );
 }
