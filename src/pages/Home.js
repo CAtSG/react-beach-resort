1e56c61ae77056d6880d4895f0c86dfd555e51ca
CAtSG <cli@studygroup.com>
2021-02-22 15:17:21 -0800

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Hero>
         <Banner
-          title="CLi's luxurious rooms"
+          title="CL's luxurious rooms"
           subtitle='deluxe rooms starting at $299'
         >
           <Link to='/rooms' className='btn-primary'>
